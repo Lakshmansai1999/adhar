a9b302f195436ceb334b93639c8959f3a826d53e
mn-stratapps <mnikhil@stratapps.com>
2021-11-23 11:06:40 +0530

    the first commit

---
+++ adhardemo/src/main/java/com/example/demoadhar/AdhardemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoadhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhardemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhardemoApplication.class, args);
+	}
+
+}
---
+++ adhardemo/src/test/java/com/example/demoadhar/AdhardemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoadhar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdhardemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
