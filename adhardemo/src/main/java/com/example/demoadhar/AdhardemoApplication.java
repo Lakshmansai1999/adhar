bdc1d9e26a209d8752a682ec5f31eb560c991a3d
mn-stratapps <mnikhil@stratapps.com>
2021-12-04 12:24:47 +0530

    commit

--- adhardemo/src/main/java/com/example/demoadhar/AdhardemoApplication.java
+++ adhardemo/src/main/java/com/example/demoadhar/AdhardemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demoadhar;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demoadhar.entity.*;
 
 @SpringBootApplication
 public class AdhardemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdhardemoApplication.class, args);
+		System.out.println("MN");
 	}
 
 }
+
+
