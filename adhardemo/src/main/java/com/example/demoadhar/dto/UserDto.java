03311a33b9093df5309f96221adec43ebbbc44e7
mn-stratapps <mnikhil@stratapps.com>
2021-12-23 22:24:21 +0530

    commit

--- adhardemo/src/main/java/com/example/demoadhar/dto/UserDto.java
+++ adhardemo/src/main/java/com/example/demoadhar/dto/UserDto.java
@@ -2,27 +2,59 @@ package com.example.demoadhar.dto;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Getter
 @Setter
 public class UserDto {
 
-	private long id;
+	private int id;
+	
+	@NotNull(message = "firstName should not be empty")
+    @Size(min = 2, message = "First Name should have atleast 2 characters")
     private String firstName;
+	
+	@NotNull(message = "lastName should not be empty")
+    @Size(min = 2, message = "last Name should have atleast 2 characters")
     private String lastName;
+	
+	@NotNull(message = "username should not be empty")
+    @Size(min = 2, message = "user Name should have atleast 2 characters and unique")
     private String username;
+	
     private String password;
-    private String email;
+    
+ /*  @NotBlank(message = "email should not be blank")
+   // @Pattern(regexp ="^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$",message="mail id is not valid")
+    @NotNull(message = "email should not be empty")
+   @Email(message = "email should be in correct format and should not be null")
+    private String email;*/
+   
+   @Pattern(regexp ="^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$",message="mail id is not valid")
+   @NotEmpty(message = "email should not be empty")
+   @Email(message = "email should be in correct format and should not be null")
+   private String email;
+    
+    @NotEmpty(message = "role should not be empty")
     private List<String> role;
+    
+    
+    public void setRole(List<String> role) {
+		this.role = role;
+	}
+    
 	@Override
 	public String toString() {
 		return "UserDto [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username=" + username
-				+ ", password=" + password + ", emailId=" + email + ", role=" + role + "]";
+				+ ", password=" + password + ", email=" + email + ", role=" + role + "]";
 	}
 
 }
